a83b4a0afc41c36bc689be1d75521a9516c77b79
ajayiezekiel <ajayiezekiel9@gmail.com>
2023-11-23 21:52:11 +0100

    Set up a react project and create a todo app

---
+++ src/components/Modal.js
@@ -0,0 +1,22 @@
+function Modal(props) {
+  function cancelHandler() {
+    props.onCancel();
+  }
+
+  function confirmHandler() {
+    props.onConfirm();
+  }
+  return (
+    <div className="modal">
+      <p>Are you sure?</p>
+      <button onClick={cancelHandler} className="btn btn--alt">
+        Cancel
+      </button>
+      <button onClick={confirmHandler} className="btn">
+        Confirm
+      </button>
+    </div>
+  );
+}
+
+export default Modal;
